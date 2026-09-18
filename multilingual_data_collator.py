d08d920135694a46234f152cfc50626847f17b6b
Michael Ginn <mginn@amazon.com>
2026-09-18 01:05:45 -0700

    Fix lbrack

--- multilingual_data_collator.py
+++ multilingual_data_collator.py
@@ -136,7 +136,7 @@ class MultilingualSelfDistillationDataCollator:
         if self.include_reference_solution_en:
             print(solution_en)
             answer_only = re.match(
-                r".*\\(\[|\()\s+\\boxed{(.*)}\s+\\(\[|\)).*",
+                r".*\\(\[|\()\s+\\boxed{(.*)}\s+\\(\]|\)).*",
                 solution_en,
                 flags=re.DOTALL,
             ).group(1)
