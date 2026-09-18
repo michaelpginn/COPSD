252161fbad3dccc14643f4cd68868f1809a4cd61
Michael Ginn <mginn@amazon.com>
2026-09-18 01:30:29 -0700

    Star

--- multilingual_data_collator.py
+++ multilingual_data_collator.py
@@ -136,7 +136,7 @@ class MultilingualSelfDistillationDataCollator:
         if self.include_reference_solution_en:
             try:
                 answer_only = re.match(
-                    r"\\boxed{(.*)}",
+                    r".*\\boxed{(.*)}.*",
                     solution_en,
                     flags=re.DOTALL,
                 ).group(1)
